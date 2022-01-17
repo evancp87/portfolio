4f485ea7afe66627e731d2c058706ddf48075cee
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-01-17 17:36:17 +0000

    moved socials to footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div className="social">
+                <ul className="social">
+                <li className="nav-item"><GitHubIcon /> https://github.com/evancp87</li>    
+                <li className="nav-item"><FacebookTwoToneIcon /> https://www.facebook.com/redrackham87</li>
+                <li className="nav-item"><InstagramIcon />https://www.instagram.com/evsparks87/?hl=en</li>
+                <li className="nav-item"><TwitterIcon />https://twitter.com/EvanPar74906861</li>
+
+                </ul>
+            </div>
+        </footer>
+    )
+
+
+}
+
+export default Footer

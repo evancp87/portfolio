f978e93f074419444a1bca4d063598f261f727b9
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-04-22 14:46:27 +0100

    new Project component for each project

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -4,14 +4,16 @@ function Project(props) {
 
     const {project} = props.projects;
     return (
-        <div key={project.title} className="project">
+        <ul>
+        <li key={project.title} className="project">
         <a href={project.link}>
           <img src={project.image} alt={project.title} className="proj-img" />
         </a>
           <h2> {project.title}</h2>
           <h3>{project.subtitle}</h3>
           <p className="proj-description">{project.description}</p>
-      </div>
+      </li>
+      </ul>
     )
 }
 

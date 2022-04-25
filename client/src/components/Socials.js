7bac2eec54134ea5078af0bbfbd18a61e7bf3534
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-04-25 15:32:16 +0100

    react-icons added in- old icons removed

--- client/src/components/Socials.js
+++ client/src/components/Socials.js
@@ -1,20 +1,48 @@
-import React from 'react'
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { TiSocialLinkedinCircular } from "react-icons/ti";
+import { IconContext } from "react-icons";
 
-const  Socials = () => {
-    return (
-        <div className="socials">
-           
-{/* <GitHubIcon/>
-<InstagramIcon/>
-<LinkedInIcon/> */}
-        </div>
-    )
+const Socials = () => {
+
+    // const style = { fontSize: "1.5em" }
+  return (
+    <div className="socials">
+      <ul >
+      <IconContext.Provider
+      value={{size: '3rem', padding: '5em' }}
+    >
+        <li socials__item>
+          <a href="https://github.com/evancp87">
+
+          <AiFillGithub />
+          </a>
+        </li>
+
+        <li socials__item>
+        <a href="https://www.instagram.com/evsparks87/">
+          <AiFillInstagram />
+          </a>
+        </li>
+
+        <li socials__item>
+        <a href="https://www.linkedin.com/in/evan-parker-9a336987/">
+          <TiSocialLinkedinCircular />
+          </a>
+        </li>
+        </IconContext.Provider>
+      </ul>
+    
+    </div>
+  );
+};
+
+// <li className="nav-item"><GitHubIcon /> https://github.com/evancp87</li>
+{
+  /* <li className="nav-item"><FacebookTwoToneIcon /> https://www.linkedin.com/in/evan-parker-9a336987/</li> */
 }
-
-// <li className="nav-item"><GitHubIcon /> https://github.com/evancp87</li>    
-{/* <li className="nav-item"><FacebookTwoToneIcon /> https://www.linkedin.com/in/evan-parker-9a336987/</li> */}
-{/* <li className="nav-item"><InstagramIcon />https://www.instagram.com/evsparks87/?hl=en</li> */}
-export default Socials
+{
+  /* <li className="nav-item"><InstagramIcon />https://www.instagram.com/evsparks87/?hl=en</li> */
+}
+export default Socials;

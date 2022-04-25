3a59ffb639a7588623b2b28568faa42da4d48241
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-04-25 15:47:22 +0100

    neon button for header section- a new component

---
+++ client/src/components/NeonButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { MdArrowRightAlt } from "react-icons/md";
+
+function NeonButton() {
+  return (
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Neon Button
+      <MdArrowRightAlt />
+    </a>
+  );
+}
+
+// https://www.youtube.com/watch?v=ex7jGbyFgpA&list=PLxdw55vilFza8S9E9YD2u-74CZH8s1SlW&index=196
+export default NeonButton;

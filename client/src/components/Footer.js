a03cee8349f13c5c648aa1b23fa49653e21aa488
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-01 20:54:35 +0100

    scroll button added and new classes for Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,28 +1,29 @@
-import React from 'react';
-// import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
-// import TwitterIcon from '@mui/icons-material/Twitter';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import {BsFillArrowUpCircleFill} from "react-icons/md";
+import React from "react";
+import ScrollButton from './ScrollButton';
 
 const Footer = () => {
-const date = new Date();
-const year = date.getFullYear();
-    return (
-        <footer className="ftr">
-            <div className="ftr-info">
-<div class="ftr__container">
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer className="ftr">
+      <div className="ftr-info">
+        <div class="ftr__container">
+          <img
+            className="ftr-avatar"
+            src={require("../assets/Images/footer-avatar.png")}
+            alt="Evan Parker avatar"
+          />
+        </div>
+        <div className="copyright">
 
-                <img className="ftr-avatar" src={require("../assets/Images/footer-avatar.png")} alt="Evan Parker avatar"/>
-</div>
-                <p className="copyright"> Evan Parker {year}</p>
-                <button className="contact-btn">Contact Me</button>
-            </div>
-                {/* <BsFillArrowUpCircleFill/> */}
-        </footer>
-    )
+        <p> Evan Parker ⓒ {year}</p>
+        <button className="contact-btn"><span>Contact Me</span></button>
+        <ScrollButton/>
+        </div>
+      </div>
+      {/* <BsFillArrowUpCircleFill/> */}
+    </footer>
+  );
+};
 
-
-}
-
-export default Footer
+export default Footer;

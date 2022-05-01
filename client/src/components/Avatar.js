abd1a6005e3924c1bb1b39defeefecd1c0308223
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-01 20:53:31 +0100

    attempt at doubled sided image implemented- still not working 100%

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React from "react";
 
-function Avatar () {
-    const profilePic = {
-        width: '300px',
-        height: 'auto',
-        borderRadius: '50%',
-        border: '10px solid black',
-       
-    }
-    return (
-
-        <div class="avatar-container">
-            <div className="card">
-
-
-
+function Avatar() {
+  const profilePic = {
+    width: "300px",
+    height: "300px",
+    borderRadius: "50%",
+    border: "10px solid black",
+    position: "absolute",
+    left: "40%",
+    top: "50%"
+  };
+  return (
+    <div class="avatar-container">
+      {/* <div className="card">
         <div className="face front">
-
-        <img style={profilePic} src={require("../assets/Images/avatar.png")} alt="Evan Parker avatar"/>
+          <img
+            style={profilePic}
+            src={require("../assets/Images/avatar.png")}
+            alt="Evan Parker avatar"
+          />
         </div>
         <div className="face back">
-
-        <img style={profilePic} src={require("../assets/Images/footer-avatar.png")} alt="Evan Parker avatar"/>
+          <img
+            style={profilePic}
+            src={require("../assets/Images/footer-avatar.png")}
+            alt="Evan Parker avatar"
+          />
         </div>
-            </div>
-
-          
-        </div>
-    )
+      </div> */}
+        <div className="card" title="Flip me" >
+    
+          <img
+            style={profilePic}
+            src={require("../assets/Images/avatar.png")}
+            alt="Evan Parker avatar"
+            className="face front"
+          />
+        
+    
+          <img
+            style={profilePic}
+            src={require("../assets/Images/footer-avatar.png")}
+            alt="Evan Parker avatar"
+            className="face back"
+          />
+        
+      </div>
+    </div>
+  );
 }
 
-export default Avatar
+
+// http://css3.bradshawenterprises.com/flip/#xandy
+export default Avatar;

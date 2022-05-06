c4a721d9c6f5e88e377053853fca957d4cda9679
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-06 15:49:42 +0100

    file with each skill

---
+++ client/src/skillsData.js
@@ -0,0 +1,67 @@
+
+const bootstrapIcon = require("./assets/Images/bootstrap.png");
+const htmlIcon = require("./assets/Images/html-5.png");
+const javascriptIcon = require("./assets/Images/javascript.png");
+const cssIcon = require("./assets/Images/css.png");
+const mongoDBIcon = require("./assets/Images/mongo-db.png");
+const nodeJSIcon = require("./assets/Images/nodejs.png");
+const reactIcon = require("./assets/Images/react.png");
+const sassIcon = require("./assets/Images/sass.png");
+const sqlIcon = require("./assets/Images/sql.png");
+
+export const SkillInfo = [
+  {
+    title: "Bootstrap",
+    image: bootstrapIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "HTML",
+    image: htmlIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "JavaScript",
+    image: javascriptIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "CSS",
+    image: cssIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "Node JS",
+    image: nodeJSIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "React",
+    image: reactIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "Sass",
+    image: sassIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "MongoDB",
+    image: mongoDBIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    title: "SQL",
+    image: sqlIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];

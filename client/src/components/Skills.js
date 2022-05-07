f4d53bb6f286db004b9199ec7d6711c547000c12
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-07 21:39:36 +0100

    new skills icons, also refactored skills into a single skill component

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,22 +1,29 @@
 import {React} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { DiJavascript1 } from "react-icons/di";
-import { DiReact } from "react-icons/di";
-import { DiMongodb } from "react-icons/di";
-import { AiFillHtml5 } from "react-icons/ai";
-import { SiCss3 } from "react-icons/si";
-import { DiSass } from "react-icons/di";
-import { BsFillBootstrapFill } from "react-icons/bs";
-import { SiJquery } from "react-icons/si";
+import Skill from './Skill';
+import {SkillInfo} from '../skillsData';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { DiJavascript1 } from "react-icons/di";
+// import { DiReact } from "react-icons/di";
+// import { DiMongodb } from "react-icons/di";
+// import { AiFillHtml5 } from "react-icons/ai";
+// import { SiCss3 } from "react-icons/si";
+// import { DiSass } from "react-icons/di";
+// import { BsFillBootstrapFill } from "react-icons/bs";
+// import { SiJquery } from "react-icons/si";
 
 
 
 const Skills = () => {
 
+   
+
     return (
 <section className="skills">
-    <h2 className="skills__header"></h2>
-    < ul className="skills__list">
+    <div className="skills__header">
+
+    <h2 >Skills</h2>
+    </div>
+    {/* < ul className="skills__list">
     <li className="skill"><DiReact/></li>
     <li className="skill"><DiJavascript1 /></li>
     <li className="skill"><DiMongodb /></li>
@@ -24,8 +31,82 @@ const Skills = () => {
     <li className="skill"><SiCss3 /></li>
     <li className="skill"><DiSass /></li>  
     <li className="skill"><BsFillBootstrapFill /></li>  
-    <li className="skill"><SiJquery /></li>  
+    <li className="skill"><SiJquery /></li>   */}
+<ul className="skills__list">
+    {SkillInfo.map((skill) => 
+    <Skill
+    image={skill.image}
+    title={skill.title}
+    description={skill.description}
+    
+    
+    />
+    
+    )}
 
+    {/* <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/bootstrap.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/css.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/html-5.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/javascript.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/nodejs.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/react.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>  
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/sass.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>  
+    <li className="skills__item"><img 
+    className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/mongo-db.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li>
+            <li className="skills__item"><img 
+            className="skills__icon"
+            // style={skill}
+            src={require("../assets/Images/sql.png")}
+            alt="Evan Parker avatar"
+            // className="face front"
+          /></li> */}
      
     </ul>
 </section>

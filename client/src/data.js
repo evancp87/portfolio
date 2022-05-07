2262821f4e8762c4119978bc1b7e2e4c84dccbda
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-07 21:37:24 +0100

    new img for mars dashboard, removed subtitles properties

--- client/src/data.js
+++ client/src/data.js
@@ -4,10 +4,10 @@
 // const homebrew = require('homebrew')
 const homebrew = require('./assets/Images/homebrew.png')
 const wyr = require('./assets/Images/would-you-rather.png')
+const mars = require('./assets/Images/mars-dashboard.png')
 export const projects = [
   {
     title: "HomeBrew",
-    subtitle: "",
     description:
       "A fictional home brewing subscription website. This was my final project a part of my General Assembly Front End Web Development course. It is a static website built with Sass and vanilla Javascript ",
     image: homebrew,
@@ -15,21 +15,18 @@ export const projects = [
   },
   {
     title: "Would You Rather",
-    subtitle: "",
     description: "A game of Would You Rather, built with React and Redux. Part of my Udacity React Nanodegree" ,
     image: wyr,
     link: "https://wyrcats.netlify.app/",
   },
   {
     title: "Mars Rover Dashboard",
-    subtitle: "",
     description: "A dashboard showing the information of 3 different Mars Rovers. Part of my Udacity Intermediate Javascript Nanodegree" ,
-    image: homebrew,
+    image: mars,
     link: "https://marsrover-udacity.netlify.app/",
   },
   {
     title: "Frontend Mentor projects",
-    subtitle: "challenges completed on the Front End Mentor website",
     description:
       "challenges completed on the Front End Mentor website",
     image: wyr,

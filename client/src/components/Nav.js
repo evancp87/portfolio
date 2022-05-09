0449260a9bb472fc48c62f5ad214e22d0d1764c4
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-09 15:35:48 +0100

    a tags added for linking to sections of page

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,33 +1,39 @@
+import { NavLink } from "react-router-dom";
 
-import {NavLink} from 'react-router-dom';
-
-
-import React from 'react';
+import React from "react";
 
 const Nav = () => {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li className="logo nav-item">Evan Parker</li>
+      </ul>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <a href="#header">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#projects">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="nav-item">
+          <a href="#about">About</a>
+        </li>
+        <li className="nav-item">
+          {/* <a href="#"> */}
+          CV
+          {/* </a> */}
+        </li>
 
-    return (
-        <nav className='navigation'>
-           <ul>
-
-            <li className="logo nav-item">Evan Parker</li>
-           </ul>
-            <ul className="nav-items">
-                <li className="nav-item">Home</li>
-                <li className="nav-item">About</li>
-                <li className="nav-item">Projects</li>
-                <li className="nav-item">CV</li> 
-                
-                {/* <li className="nav-item"><NavLink to='/' exact activeClassName='active'>Home</NavLink></li>
+        {/* <li className="nav-item"><NavLink to='/' exact activeClassName='active'>Home</NavLink></li>
                 <li className="nav-item"><NavLink to='/about'  activeClassName='active'>About</NavLink></li>
                 <li className="nav-item"><NavLink to='/projects'  activeClassName='active'>Projects</NavLink></li>
                 <li className="nav-item"><NavLink to='/cv'  activeClassName='active'>CV</NavLink></li> */}
-                
-            </ul>
-        </nav>
-    )
+      </ul>
+    </nav>
+  );
+};
 
-
-}
-
-export default Nav
+export default Nav;

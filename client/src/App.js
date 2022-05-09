5fff421fd410343a2e5e3cf31d4fa17eb1d08a99
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-09 17:23:03 +0100

    chore: code tidy of scss files, formatting, removing commented out code, etc.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-// // import "../styles/style.css"
-// import "./styles"
+
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Projects from "./components/Projects";

a2102fbcd675fd4506fb23f62c003d4e67f9f9ec
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-05-09 15:39:13 +0100

    tweaks to scroll function on scroll button

--- client/src/components/ScrollButton.js
+++ client/src/components/ScrollButton.js
@@ -1,40 +1,44 @@
-import React, {useState} from 'react'
-import {BsFillArrowUpCircleFill} from 'react-icons/bs';
+import React, { useState } from "react";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 function ScrollButton() {
+  const [visible, setVisible] = useState(false);
 
-    const [visible, setVisible] = useState(false);
+  function toggleVisible() {
+    const scrolled = document.documentElement.scrollTop;
 
-    function toggleVisible () {
-        const scrolled = document.documentElement.scrollTop;
-
-        if (scrolled > 3400) {
-          return  setVisible(true);
-        } else {
-            return setVisible(false);
-        }
+    if (scrolled > 3400) {
+      return setVisible(true);
+    } else {
+      return setVisible(false);
     }
+  }
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            overflowY: 'scroll',
-            behavior: 'smooth'
-        })
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      overflowY: "scroll",
+      behavior: "smooth",
+    });
+  };
 
-    }
+  window.addEventListener("scroll", toggleVisible);
 
-    window.addEventListener('scroll', toggleVisible);
-
-    return (
-        <button className="scroll-btn"
-        onClick={scrollToTop}
-        style={{display: visible ? 'inline' : 'none' ,border: 'none', background: 'none', width: '150px', height: 'auto', transition: '1s'}}
-        
-        >
-          <BsFillArrowUpCircleFill 
-          />  
-        </button>
-    )
+  return (
+    <button
+      className="scroll-btn"
+      onClick={scrollToTop}
+      style={{
+        display: visible ? "inline" : "none",
+        border: "none",
+        background: "none",
+        width: "150px",
+        height: "auto",
+        transition: "1s",
+      }}
+    >
+      <BsFillArrowUpCircleFill className="scroll-button-arrow" />
+    </button>
+  );
 }
 
-export default ScrollButton
+export default ScrollButton;

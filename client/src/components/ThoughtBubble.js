c56fb68c408e453775ba9d79d0ba8b07d29847ac
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-09-14 19:04:50 +0100

    THought bubble component for header

---
+++ client/src/components/ThoughtBubble.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ThoughtBubble() {
+  return (
+    <div class="thought-bubble">
+      <div class="thought-bubble__point"></div>
+      <p class="thought-bubble__txt">
+        "I wonder what would happen if someone were to click my face?"
+      </p>
+    </div>
+  );
+}

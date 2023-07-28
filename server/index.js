06a509a62bcd32122f83344b116e864d1c6419a4
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-28 08:16:50 +0100

    tweaks to server file paths

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 // server/index.js
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 2000;
-
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../client/dist/build")));
+app.use(express.json());
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/", "index.html"));
 });
 
 app.listen(PORT, () => {

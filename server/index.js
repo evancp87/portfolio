15c002a613c2612a46273f46da2b9838bad92940
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-31 11:41:03 +0000

    update

--- server/index.js
+++ server/index.js
@@ -9,9 +9,19 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../client/dist/build")));
 app.use(express.json());
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/dist/build", "index.html"));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../client/dist/build", "index.html"));
+// });
+
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

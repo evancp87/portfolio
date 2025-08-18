611e8c34f80637270851fc7ab38a47aad04489fc
Evan Parker <65721884+evancp87@users.noreply.github.com>
2025-08-18 22:31:11 +0100

    Feat: upgrade dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 // import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App.jsx";
-import store from "./store/store.js";
-import "./styles/style.css";
-import reportWebVitals from "./reportWebVitals.js";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './store/store.js';
+import './styles/style.css';
+import reportWebVitals from './reportWebVitals.js';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
